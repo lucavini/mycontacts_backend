1648021cda065971fb26dfc698389849457dd063
Lucas Santos <lucas.santos@mblabs.com.br>
2023-02-11 17:27:21 -0300

    feat: contact repository functions

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -31,11 +31,41 @@ class ContactRepository {
     });
   }
 
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  create(contact) {
+    return new Promise((resolve) => {
+      const newContact = {
+        ...contact,
+        id: v4(),
+      };
+      contacts = [...contacts, newContact];
+
+      resolve(newContact);
+    });
+  }
+
+  update(id, contact) {
+    return new Promise((resolve) => {
+      let updatedContact = contacts.find((con) => con.id === id);
+      updatedContact = {
+        ...contact,
+        id,
+      };
+
+      contacts = contacts.map((con) => (con.id === id ? updatedContact : con));
+
+      resolve(updatedContact);
+    });
+  }
+
   deleteById(id) {
     return new Promise((resolve) => {
-      resolve(
-        contacts = contacts.filter((contact) => contact.id !== id),
-      );
+      resolve((contacts = contacts.filter((contact) => contact.id !== id)));
     });
   }
 }
