ae82632d84afade6b2e95ed77165fed957ec1a38
lucas santos <lucas.santos@mblabs.com.br>
2023-02-19 10:16:49 -0300

    feat: set default port

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3333, () => console.log('Server started at http://localhost:3333'));
