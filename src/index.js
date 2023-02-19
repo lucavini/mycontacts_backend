30a3139339bdd30ef9adc045586b4986caf31ac3
lucas santos <lucas.santos@mblabs.com.br>
2023-02-19 14:40:55 -0300

    featc: cors middleware

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ require('express-async-errors');
 
 const routes = require('./routes');
 const errorHandler = require('./app/middlewares/errorHandler');
+const cors = require('./app/middlewares/cors');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
